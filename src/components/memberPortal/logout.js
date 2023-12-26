549c5a4adf86ee4517ebc104c8c43d9ccb715ea4
MarkOfosu <124046783+MarkOfosu@users.noreply.github.com>
2023-12-26 00:36:38 -0800

    created a reusable tooltip

--- src/components/memberPortal/logout.js
+++ src/components/memberPortal/logout.js
@@ -1,7 +1,7 @@
 // src/components/Logout.js
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../../src/context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import './logout.css';
 
 const Logout = () => {
